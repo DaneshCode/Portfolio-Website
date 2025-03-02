fc0f844a69a554c10310323a360676cdf1e4c81d
Danesh Khodadad <dani.kh2652@gmail.com>
2025-03-02 22:49:35 +0330

    update

--- main.js
+++ main.js
@@ -1,99 +1,99 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
   });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
   });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll(".nav__link");
+const navLink = document.querySelectorAll('.nav__link');
 navLink.forEach((a) =>
-  a.addEventListener("click", () => {
-    const navMenu = document.getElementById("nav-menu");
+  a.addEventListener('click', () => {
+    const navMenu = document.getElementById('nav-menu');
     // when user click on each nav__link , remove the show-menu class
-    navMenu.classList.remove("show-menu");
-  })
+    navMenu.classList.remove('show-menu');
+  }),
 );
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName("skills__content"),
-  skillsHeader = document.querySelectorAll(".skills__header");
+const skillsContent = document.getElementsByClassName('skills__content'),
+  skillsHeader = document.querySelectorAll('.skills__header');
 
 skillsHeader.forEach((element) => {
-  element.addEventListener("click", function toggleSkills() {
-    if (this.parentNode.className === "skills__content skills__open") {
-      this.parentNode.className = "skills__content skills__close";
+  element.addEventListener('click', function toggleSkills() {
+    if (this.parentNode.className === 'skills__content skills__open') {
+      this.parentNode.className = 'skills__content skills__close';
     } else {
-      this.parentNode.className = "skills__content skills__open";
+      this.parentNode.className = 'skills__content skills__open';
     }
   });
 });
 /*==================== QUALIFICATION TABS ====================*/
-const educationBtn = document.getElementById("education-btn"),
-  workBtn = document.getElementById("work-btn"),
-  educationDiv = document.getElementById("education"),
-  workDiv = document.getElementById("work");
+const educationBtn = document.getElementById('education-btn'),
+  workBtn = document.getElementById('work-btn'),
+  educationDiv = document.getElementById('education'),
+  workDiv = document.getElementById('work');
 
-educationBtn.addEventListener("click", function showEducations() {
-  educationBtn.style.color = "#6e57e0";
-  workBtn.style.color = "#6d6a7c";
-  educationDiv.className = "ualification__content qualification__active";
-  workDiv.className = "qualification__content";
+educationBtn.addEventListener('click', function showEducations() {
+  educationBtn.style.color = '#6e57e0';
+  workBtn.style.color = '#6d6a7c';
+  educationDiv.className = 'ualification__content qualification__active';
+  workDiv.className = 'qualification__content';
 });
 
-workBtn.addEventListener("click", function showWorks() {
-  workBtn.style.color = "#6e57e0";
-  educationBtn.style.color = "#6d6a7c";
-  workDiv.className = "ualification__content qualification__active";
-  educationDiv.className = "qualification__content";
+workBtn.addEventListener('click', function showWorks() {
+  workBtn.style.color = '#6e57e0';
+  educationBtn.style.color = '#6d6a7c';
+  workDiv.className = 'ualification__content qualification__active';
+  educationDiv.className = 'qualification__content';
 });
 
 /*==================== SERVICES MODAL ====================*/
-const modalViews = document.querySelectorAll(".services__modal"),
-  modalBtns = document.querySelectorAll(".services__button"),
-  modalCloses = document.querySelectorAll(".services__modal-close");
+const modalViews = document.querySelectorAll('.services__modal'),
+  modalBtns = document.querySelectorAll('.services__button'),
+  modalCloses = document.querySelectorAll('.services__modal-close');
 
 let modal = function (modalClick) {
-  modalViews[modalClick].classList.add("active-modal");
+  modalViews[modalClick].classList.add('active-modal');
 };
 
 modalBtns.forEach((modalBtn, i) => {
-  modalBtn.addEventListener("click", () => {
+  modalBtn.addEventListener('click', () => {
     modal(i);
   });
 });
 
 modalCloses.forEach((modalClose) => {
-  modalClose.addEventListener("click", () => {
+  modalClose.addEventListener('click', () => {
     modalViews.forEach((modalView) => {
-      modalView.classList.remove("active-modal");
+      modalView.classList.remove('active-modal');
     });
   });
 });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-var swiper = new Swiper(".portfolio__container", {
+var swiper = new Swiper('.portfolio__container', {
   cssMode: true,
   loop: true,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
   mousewheel: true,
@@ -125,46 +125,46 @@ var swiper = new Swiper(".portfolio__container", {
 
 /*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
-  const scrollUp = document.getElementById("scroll-up");
-  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-  else scrollUp.classList.remove("show-scroll");
+  const scrollUp = document.getElementById('scroll-up');
+  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
 }
-window.addEventListener("scroll", scrollUp);
+window.addEventListener('scroll', scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "uil-sun";
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
 
 // previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
 // we obtain the current theme that the interface has by validating the dark-them
 const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
 const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+  themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
 
 // we validate if the user previously chose a topic
 if (selectedTheme) {
   // if the validation is fulfilled, we ask what the issue was to know if we activated
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme,
   );
-  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
-    iconTheme
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](
+    iconTheme,
   );
 }
 
 // activate / deactivate the theme manually with the button
-themeButton.addEventListener("click", () => {
+themeButton.addEventListener('click', () => {
   // add or remove the dark / icon theme
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
   // we save the theme and the current icon that the user chose
-  localStorage.setItem("selected-themee", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
+  localStorage.setItem('selected-themee', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
 });
 
 // ------ TYPICAL ------
@@ -173,7 +173,7 @@ var TxtType = function (el, toRotate, period) {
   this.el = el;
   this.loopNum = 0;
   this.period = parseInt(period, 10) || 2000;
-  this.txt = "";
+  this.txt = '';
   this.tick();
   this.isDeleting = false;
 };
@@ -188,7 +188,7 @@ TxtType.prototype.tick = function () {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
   var that = this;
   var delta = 200 - Math.random() * 100;
@@ -200,7 +200,7 @@ TxtType.prototype.tick = function () {
   if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === "") {
+  } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
@@ -212,17 +212,17 @@ TxtType.prototype.tick = function () {
 };
 
 window.onload = function () {
-  var elements = document.getElementsByClassName("typewrite");
+  var elements = document.getElementsByClassName('typewrite');
   for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute("data-type");
-    var period = elements[i].getAttribute("data-period");
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
     if (toRotate) {
       new TxtType(elements[i], JSON.parse(toRotate), period);
     }
   }
   // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  var css = document.createElement('style');
+  css.type = 'text/css';
+  css.innerHTML = '.typewrite > .wrap { border-right: 0.08em solid #fff}';
   document.body.appendChild(css);
 };
